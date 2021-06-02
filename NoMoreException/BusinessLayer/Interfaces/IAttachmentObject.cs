f600bdeb97843aba73cc71f5df421be3d095e3f5
Humeyra.Can <Humeyra.Can@logo.com.tr>
2021-06-02 16:55:41 +0300

    MAD-80

---
+++ NoMoreException/BusinessLayer/Interfaces/IAttachmentObject.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Dtos;
+using DataAccess.DataModels;
+using Shared.BaseTypes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IAttachmentObject : IBusinessObject
+    {
+        AttachmentDto GetById(int id);
+        void Remove(Attachment Attachment);
+        void RemoveById(int id);
+        List<AttachmentDto> GetAttachmentsByPostId(int postId);
+        List<AttachmentDto> GetAttachmentsByCommentId(int commentId);
+        Task UpdateAttachment(Attachment Attachment);
+        void CreateAttachment(Attachment newAttachment);
+    }
+}
